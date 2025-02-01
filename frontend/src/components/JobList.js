388ceaae51f7bd75d022950637c6849b6fca9d2b
Yuval Shpitzer <shpitzery10@gmail.com>
2025-02-01 16:17:33 +0200

    last fixs in front

--- frontend/src/components/JobList.js
+++ frontend/src/components/JobList.js
@@ -16,12 +16,12 @@ function JobList({ jobs }) {
     }
 
   return (
-    <div style={{ width: "80%", margin: "auto" }}>
+    <div style={{ width: "80%", maxWidth: "1200px", margin: "auto" }}>
       <h2 style={{ textAlign: "center", marginBottom: "20px" }}>Featured Jobs</h2>
       <div style={{
         border: "1px solid #ddd",
         borderRadius: "10px",
-        overflow: "hidden",
+        overflow: "hidden", // Ensures child elements don't cross the border
       }}>
         {jobs.map((job, index) => (
           <div key={index} style={{
@@ -30,13 +30,11 @@ function JobList({ jobs }) {
             padding: "20px",
             borderBottom: "1px solid #ddd",
             backgroundColor: index % 2 === 0 ? "#fff" : "#f9f9f9",
-            // gap: "24px",
             fontSize: "16px",
-            paddingTop: "20px",
-            paddingBottom: "20px"
+            textWrap: "wrap",
           }}>
             {/* Company Logo */}
-            <div style={{ width: "40px", height: "40px", paddingLeft: "15px", paddingRight: "20px" }}>
+            <div style={{ width: "40px", height: "40px", padding: "0 20px" }}>
               <img
                 src={`https://cdn.simpleicons.org/${job.company.toLowerCase()}`}
                 alt={`${job.company} logo`}
@@ -45,13 +43,12 @@ function JobList({ jobs }) {
             </div>
 
             {/* Job Details */}
-            <div style={{ flex: 1 }}> {/* Allows the details to expand and fill available space */}
+            <div style={{ flex: "1 1 auto" }}> {/* Allows the details to expand and fill available space */}
               <a
                 href={job.url}
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{
-                //   fontSize: "16px",
                   fontWeight: "bold",
                   textDecoration: "none",
                   color: "#007bff"
@@ -65,23 +62,19 @@ function JobList({ jobs }) {
 
             {/* Location */}
             <div style={{
-              width: "150px",
+              flex: "0 1 150px",
               textAlign: "left",
-              paddingRight: "50px",
-            //   paddingLeft: "20px"
+              padding: "0 35px",
             }}>
-                {job.location.endsWith('ISR')
-                ? job.location.split(',')[0]
-                : job.location}
+                {job.location.endsWith('ISR') ? job.location.split(',')[0] : job.location}
             </div>
 
             {/* Posted Date */}
             <div style={{ 
-              width: "200px", 
-            //   fontSize: "14px", 
+              flex: "0 1 200px",
               color: "#666",
               textAlign: "left",
-              marginRight: "-35px"
+              // padding: "0 20px",
             }}>
               {dateForm(job.posted_date)}
             </div>
